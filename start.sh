58804537cc90325e5bc0e03ca117a10c6d87059e
magnoliAHAH <137452290+magnoliAHAH@users.noreply.github.com>
2025-02-15 22:40:51 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,7 +13,7 @@ while [[ $(kubectl get pods -l app=biocad-web -o 'jsonpath={..status.phase}' | g
         sleep 0.5  
         TIMEOUT=$((TIMEOUT - 1))
         if [[ $TIMEOUT -le 0 ]]; then
-            echo -e "\nError: Pods starting tiomeout."
+            echo -e "\nError: Pods starting timeout."
             exit 1
         fi
     done
@@ -21,4 +21,10 @@ done
 
 echo -e "\rAll pods are ready!   "  
 
-kubectl port-forward --address 0.0.0.0 service/biocad-web-service 32777:32777
+echo "Checking service URL..."
+SERVICE_URL=$(minikube service biocad-web-service --url)
+echo "Service is accessible at: $SERVICE_URL"
+
+echo "If you want to enable port forwarding run:"
+echo "kubectl port-forward --address 0.0.0.0 service/biocad-web-service 32777:32777"
+
